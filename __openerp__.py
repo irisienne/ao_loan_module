3e6a61c31b551aaf82ac8df043c642d990532ea8
irisienne <roumani.rahma@mail.com>
2016-05-02 14:17:36 +0100

    imp: module squelette

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'agilorg loan managment',
+    'version': '1.0.0',
+    'category': 'lone managment',
+    'sequence': 3,
+    'author': 'rahma',
+    'description': """
+    """,
+    'depends': ["base"],
+    #'data': [
+        #],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
